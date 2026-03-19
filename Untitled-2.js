192042c957cbbdb68f9e5e37605194c88ba8e62e
okadaryuuki <okadarixyuuki@gmail.com>
2026-03-19 15:14:31 +0900

    Untitled-2.js

---
+++ Untitled-2.js
@@ -0,0 +1,10 @@
+
+let score = [66,66,66,66];
+function endgame(){
+    for(let i = 0; i < 4; i++) {
+        if (score[i] <= 0) {
+            alert("player" + i + "ゲーム終了");
+            location.reload();
+        }
+    }
+}
