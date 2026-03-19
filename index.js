83ab4c0820ba2f2a31c70b494cf706ae90e8665c
subaruncc <kss-23210141@nsgcl.jp>
2026-03-19 15:32:07 +0900

    片山　jsファイル改変３

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ deck = [];
 player = [[],[],[],[]];
 fiald = [[],[],[],[]];
 play = [];
+let score = [66,66,66,66];
 
 const f = document.getElementById('fiald');
 const h = document.getElementById('hand');
@@ -60,3 +61,38 @@ function onCard(card, player) {
 }
 
 
+function cpuplay() {
+    for(let i = 1; i < 4; i++) {
+        let hand=player[i];
+        let index = Math.floor(Math.random()*hand.length);
+        let card = hand.splice(index,1)[0];
+        play.push({player:i,card:card});
+    }
+}
+
+function createDeck() {
+    for(let i = 1; i < 104; i++) {
+        let bull = 1;
+        if(i % 55 == 0) bull = 7;
+        if(i % 11 == 0) bull = 5;
+        if(i % 10 == 0) bull = 3;
+        if(i % 5 == 0) bull = 2;
+    
+        deck.push({number:i,bull:bull});
+    }  
+}
+
+function shahullDeck() {
+    deck = [*1..n]
+    deck.sort_by!{rand}
+    p deck
+}
+
+function endgame(){
+    for(let i = 0; i < 4; i++) {
+        if (score[i] <= 0) {
+            alert("player" + i + "ゲーム終了");
+            location.reload();
+        }
+    }
+}
