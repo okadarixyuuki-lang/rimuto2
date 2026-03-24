e2ecf5c4a325dea952e574fafb84ee24d00b327f
okadaryuuki <okadarixyuuki@gmail.com>
2026-03-24 14:19:24 +0900

    okada-kaihenn

--- Untitled-2.js
+++ Untitled-2.js
@@ -2,7 +2,7 @@
 let score = [66,66,66,66];
 function endgame(){
     for(let i = 0; i < 4; i++) {
-        if (score[i] <= 0) {
+        if (score[i] <=0 0) {
             alert("player" + i + "ゲーム終了");
             location.reload();
         }
